c28b8fcd0bf10a2ea802e5c3efb241e2f42e5953
Mary Beth Snodgrass <mbs@b-a-s-s-consulting.com>
2018-10-14 17:33:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+def fizzbuzz
+  x=0
+  while x < 100
+  x += 1
+    if x % 3 == 0 && x % 5 == 0 
+      puts "fizzbuzz"
+    elsif x % 3 == 0 
+       puts "fizz"
+    elsif x % 5 == 0 
+       puts "buzz"
+    else puts x 
+    end
+  end
+end
+
+fizzbuzz
